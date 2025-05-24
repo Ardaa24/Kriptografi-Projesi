b7cea280651189d66467d4717b4895a51e0362dc
Arda24 <92623903+Ardaa24@users.noreply.github.com>
2025-05-24 10:29:44 +0300

    Add files via upload

---
+++ decryptor_ucontrol.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KriptografiProjesi.UControl
+{
+    public partial class decryptor_ucontrol : UserControl
+    {
+        public decryptor_ucontrol()
+        {
+            InitializeComponent();
+        }
+
+        //Decryptor UControl
+        string normalYazi;
+        string sifreliYazi;
+
+        private void coz_button_Click(object sender, EventArgs e)
+        {
+            sifreliYazi = sifre_textbox.Text;
+            byte[] bytes = Convert.FromBase64String(sifreliYazi);
+            normalYazi = Encoding.UTF8.GetString(bytes);
+            normal_textbox.Text = normalYazi;
+
+
+            //Eğer normal metin boşsa hata mesajı göster.
+            if (string.IsNullOrEmpty(normalYazi))
+            {
+                MessageBox.Show("Lütfen şifreli metni giriniz.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                //MessageBox.Show("Şifre çözme işlemi başarılı!", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            
+            
+            }
+        }
+    }
---
+++ encryptor_UControl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KriptografiProjesi.UControl
+{
+    public partial class encryptor_UControl: UserControl
+    {
+        public encryptor_UControl()
+        {
+            InitializeComponent();
+        }
+
+        //Encryptor UControl
+        string normalYazi;
+        string sifreliYazi;
+        private void normal_textbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sifrele_button_Click(object sender, EventArgs e)
+        {
+
+            normalYazi = normal_textbox.Text;
+
+
+            byte[] bytes = ASCIIEncoding.UTF8.GetBytes(normalYazi);
+
+            sifreliYazi = Convert.ToBase64String(bytes);
+            sifre_textbox.Text = sifreliYazi;
+
+            //Eğer şifreli metin boşsa hata mesajı göster.
+            if (string.IsNullOrEmpty(sifreliYazi))
+            {
+                MessageBox.Show("Lütfen normal metni giriniz.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
